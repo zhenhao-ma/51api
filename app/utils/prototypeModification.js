27cc0b99aff4937c780d163b028d207a56a09cb6
Darevil <zmabob@gmail.com>
2019-11-22 11:51:14 +0800

    fixed a coffliction between our Object.prototype modification and the node-package uuid

--- app/utils/prototypeModification.js
+++ app/utils/prototypeModification.js
@@ -1,9 +1,12 @@
 
 Object.prototype.hasKeys = function (keys) {
-    for (let k = 0; k < keys.length; k++) {
-        if (!this.hasOwnProperty(keys[k])) {
-            throw new Error('缺少键值对：' + keys[k]);
+    console.log('keys: ', keys);
+    if (Array.isArray(keys)) {
+        for (let k = 0; k < keys.length; k++) {
+            if (!this.hasOwnProperty(keys[k])) {
+                throw new Error('缺少键值对：' + keys[k]);
+            }
         }
+        return true
     }
-    return true
 };
