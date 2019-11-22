963167c0f0cc13c803214bde46a7ea258b84d572
Darevil <zmabob@gmail.com>
2019-11-22 16:44:09 +0800

    now we have string.isUuid4() => bool

--- app/utils/prototypeModification.js
+++ app/utils/prototypeModification.js
@@ -1,6 +1,6 @@
+const uuidv4 = require('uuid/v4');
 
 Object.prototype.hasKeys = function (keys) {
-    console.log('keys: ', keys);
     if (Array.isArray(keys)) {
         for (let k = 0; k < keys.length; k++) {
             if (!this.hasOwnProperty(keys[k])) {
@@ -9,4 +9,9 @@ Object.prototype.hasKeys = function (keys) {
         }
         return true
     }
+};
+
+String.prototype.isUuid4 = function () {
+    let regex = RegExp('^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$\n');
+    return regex.test(this)
 };
