1b308550c41577b438b3916ef7d75a844107df88
Darevil <zmabob@gmail.com>
2019-12-10 23:41:18 +0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,12 +3,13 @@ let path = require('path');
 let prototypeModification = require('./utils/prototypeModification');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let cors = require('cors');
 
 let routers = require('./routes');
 // let usersRouter = require('./routes/socialMedia/users');
 
 let app = express();
-
+app.options('*', cors()); // enable pre-flight across-the-board
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
